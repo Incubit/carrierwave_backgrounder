c4ec482f3ae9291e5c3f06b88bd4b7365b9af149
naobit <kitamura.n@incubit.co.jp>
2017-07-17 18:28:30 +0900

    fix for fog

--- lib/backgrounder/workers/store_asset_mixin.rb
+++ lib/backgrounder/workers/store_asset_mixin.rb
@@ -19,7 +19,14 @@ module CarrierWave
           record.send :"process_#{column}_upload=", true
           record.send :"#{column}_tmp=", nil
           record.send :"#{column}_processing=", false if record.respond_to?(:"#{column}_processing")
-          File.open(cache_path) { |f| record.send :"#{column}=", f }
+
+          # Download cloud temp image before processing
+          require 'open-uri'
+          File.open(cache_path, 'wb') do |output|
+            output << open(@cloud_cache_path).read
+            record.send :"#{column}=", output
+          end
+
           if record.save!
             FileUtils.rm_r(tmp_directory, :force => true)
           end
@@ -35,6 +42,17 @@ module CarrierWave
         cache_directory  = File.expand_path(asset.cache_dir, asset.root)
         @cache_path      = File.join(cache_directory, asset_tmp)
         @tmp_directory   = File.join(cache_directory, asset_tmp.split("/").first)
+
+        # Set cloud dir for temp
+        @cloud_cache_path  = File.join(CarrierWave::Uploader::Base.asset_host, CarrierWave::Uploader::Base.fog_directory, asset.cache_dir, asset_tmp)
+
+        # Create a dir if not exists
+        dirname = File.dirname(@cache_path)
+        tokens = dirname.split(/[\/\\]/)
+        1.upto(tokens.size) do |n|
+          dir = "/" + tokens[1..n].join("/")
+          Dir.mkdir(dir) unless Dir.exist?(dir)
+        end
       end
 
     end # StoreAssetMixin
