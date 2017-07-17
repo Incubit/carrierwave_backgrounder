8e613110a6c8b819d6ec21fc9999f1bf25804337
naobit <kitamura.n@incubit.co.jp>
2017-07-17 19:23:35 +0900

    update loggers

--- lib/backgrounder/workers/store_asset_mixin.rb
+++ lib/backgrounder/workers/store_asset_mixin.rb
@@ -45,15 +45,9 @@ module CarrierWave
 
         # Set cloud dir for temp
         @cloud_cache_path  = File.join(CarrierWave::Uploader::Base.asset_host, asset.cache_dir, asset_tmp)
-        puts "====worker.store_directories============="
         Rails.logger.error "====worker.store_directories============="
-
-        puts @cloud_cache_path
-        Rails.logger.error @cloud_cache_path
-
-
-        puts "====worker.store_directories============="
-        Rails.logger.error "====worker.store_directories============="
+        Rails.logger.error @cloud_cache_path, @cloud_cache_path
+        Rails.logger.error @cache_path, @cache_path
 
         # Create a dir if not exists
         dirname = File.dirname(@cache_path)
